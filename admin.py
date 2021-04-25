5fced278374798d434b157137018d5f5e0a6f35c
rnic92 <61059439+rnic92@users.noreply.github.com>
2021-04-25 16:39:00 -0400

    Add files via upload
    admin stuff

---
+++ admin.py
@@ -0,0 +1,9 @@
+import requests
+from hashlib import sha256
+typ = input("enter type [Doctor/Patient]")
+name = input("enter name")
+psw = sha256(input("enter pw").encode()).hexdigest()
+
+data = {"user":name, "pass":psw}
+r = requests.post(("http://127.0.0.1/Admin/" + typ), json=data)
+print(r.text)
