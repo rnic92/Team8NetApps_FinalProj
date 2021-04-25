4e667211f62714519114ab9b83080abd765820c1
SweetSuttons <joshs98@vt.edu>
2021-04-25 16:41:34 -0400

    V2.1

--- admin.py
+++ admin.py
@@ -5,5 +5,5 @@ name = input("enter name: ")
 psw = sha256(input("enter pw").encode()).hexdigest()
 
 data = {"user":name, "pass":psw}
-r = requests.post(("http://127.0.0.1/Admin/" + typ), json=data)
+r = requests.post(("http://127.0.0.1:8081/Admin/" + typ), json=data)
 print(r.text)
