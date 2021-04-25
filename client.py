fbad232da9514617cb80a74c9fe2d232634b05e1
NTR <rnic92@vt.edu>
2021-04-25 15:22:06 -0400

    initial client

---
+++ client.py
@@ -0,0 +1,11 @@
+
+
+
+if __name__ == '__main__':
+    usertype = input("Are you a medical professional? [y/n]")
+    if usertype.lower() == 'y' or usertype.lower() == 'yes':
+        doctor = True
+    else
+        doctor = False
+
+    
