f93473fc855b34a1466593917e2112f08a469c91
rnic92 <rnic92@vt.edu>
2021-04-25 17:52:34 -0400

    qr code reader/generator

---
+++ qrReader.py
@@ -0,0 +1,26 @@
+import qrcode
+import cv2
+import time
+
+def generate_qr(name):
+    randtag = str(time.time()) + name
+    filename = "testfile.png"
+    img = qrcode.make(randtag)
+    img.save(filename)
+
+def read_qr():
+    cap = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
+    while True:
+        ret,frame = cap.read()
+        cv2.imshow('frame', frame)
+        data, bbox, straight_qrcode = detector.detectAndDecode(frame)
+        if straight_qrcode is not None:
+            return data
+            cap.release()
+            cv2.destroyAllWindows()
+            break;
+
+if __name__ == '__main__':
+    img = generate_qr("nicolas")
+    read_qr()
